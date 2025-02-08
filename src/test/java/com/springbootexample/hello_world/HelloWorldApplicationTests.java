ee58cec09be94a74bcfed2112b5a653b126dc640
Bala Ganesh <balaganeshk3008@gmail.com>
2025-02-08 12:57:49 +0530

    parallel test

--- src/test/java/com/springbootexample/hello_world/HelloWorldApplicationTests.java
+++ src/test/java/com/springbootexample/hello_world/HelloWorldApplicationTests.java
@@ -3,13 +3,13 @@ package com.springbootexample.hello_world;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.fail;
+//import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 class HelloWorldApplicationTests {
 
     @Test
     void contextLoads() {
-        fail("Forcing test failure in GitHub Actions. ");
+       // fail("Forcing test failure in GitHub Actions. ");
     }
 }
